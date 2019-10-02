d0854d95ad7c7f8123577dea442a0b021829c0a0
GeorgeChan <george_95@126.com>
2019-10-02 21:41:25 +0800

    [Feature] 配置中心客户端添加动态刷新

--- config-client/src/main/java/com/george/controller/ConfigClientController.java
+++ config-client/src/main/java/com/george/controller/ConfigClientController.java
@@ -1,18 +1,27 @@
 package com.george.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *
+ *  @RefreshScope 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+ *                  手动刷新配置：当配置文件发生改动的时候，需要访问接口  curl -X POST http://localhost:9001/actuator/refresh
+ *                  [
+ *                      "config.client.version",
+ *                      "test.env"
+ *                  ]
+ *                  表示刷新成功！
+ *                  localhost:9001 客户端ip和端口
  * </p>
  *
  * @author GeorgeChan 2019/10/2 17:33
  * @version 1.0
  * @since jdk1.8
  */
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
